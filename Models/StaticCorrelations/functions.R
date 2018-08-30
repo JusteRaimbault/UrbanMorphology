bd54683fd5eaa1e195c040f46a43d7d632e45f17
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-08-30 12:15:26 +0200

    get_morpho_data

---
+++ Models/StaticCorrelations/functions.R
@@ -0,0 +1,164 @@
+
+library(raster)
+library(ggplot2)
+library(dplyr)
+library(rgdal)
+library(rgeos)
+library(reshape2)
+
+
+# various functions
+
+#'
+#' converts a dataframe with two first columns coords into a raster (value third col)
+dfToRaster <-function(d,column=3,normalize=FALSE){
+  d=d[!is.na(d[,3]),]
+  r=raster(SpatialPixels(SpatialPoints(d[,c(1,2)])))
+  vals = d[,column]
+  #show(length(vals))
+  #show(ncell(r))
+  if(normalize==TRUE){vals=(vals-min(vals))/(max(vals)-min(vals))}
+  return(setValues(r,vals,index=cellFromXY(r,d[,c(1,2)])))
+}
+
+
+
+#'
+#' compute cross correlations between two set of cols
+#'
+crossCorrelations<-function(m,c1,c2,f=function(x,y){return(cor(x,y))}){
+  res = matrix(0,length(c1),length(c2))
+  for(i in 1:length(c1)){
+    for(j in 1:length(c2)){
+      res[i,j]=f(m[,c1[i]],m[,c2[j]])
+    }
+  }
+  return(res)
+}
+
+
+
+#'
+#' @description compute correlation matrices, with a given function (use corrTest for estimates and confidence intervals)
+#'      on square windows centered on (xcors)x(ycors), of size xyrhoasize (must be in coordinates units)
+#'
+getCorrMatrices<-function(xcors,ycors,step,rhoasize,res,f=function(m){ifelse(is.na(m),matrix(NA,ncol(res)-2,ncol(res)-2),cor(m[,c(-1,-2)]))}){
+  xstep=diff(xcors)[1];ystep=diff(ycors)[1]
+  xrhoasize = xstep/step*rhoasize
+  yrhoasize = ystep/step*rhoasize
+  
+  allxcoords=list();allycoords=list()
+  
+  corrs=list()
+  for(i in 1:length(xcors)){
+    corrs[[i]]=list();allxcoords[[i]]=list();allycoords[[i]]=list()
+    show(i)
+    for(j in 1:length(ycors)){
+      # compute correlation matrix ?
+      x=xcors[i];y=ycors[j]
+      rows = abs(res[,1]-x)<xrhoasize/2&abs(res[,2]-y)<yrhoasize/2
+      rho = f(NA)
+      #rho = matrix(NA,nrow(res)-2,ncol(res)-2)
+      #rho = list()#atrix(NA,(ncol(res)-2),(ncol(res)-2))
+      if(length(which(rows))>10){# arbitrary threshold to have a minimal quantity of measures
+        rho = cor(res[rows,c(-1,-2)])
+        #rho = f(res[rows,])
+        corrs[[i]][[j]]=rho
+        allxcoords[[i]][[j]]=x;allycoords[[i]][[j]]=y
+      }
+    }
+  }
+  
+  res=list()
+  res$corrs = corrs
+  res$xcors = allxcoords;res$ycors = allycoords
+  
+  return(res)
+}
+
+
+#'
+#' @description given list of correlations matrices corrs, computes a function of this matrix at coordinates (xcors)x(ycors)
+getCorrMeasure<-function(xcors,ycors,corrs,f){
+  res = matrix(0,length(xcors)*length(ycors),3)
+  k=1
+  for(i in 1:length(xcors)){
+    show(i)
+    for(j in 1:length(ycors)){
+      x=xcors[i];y=ycors[j]
+      res[k,1:2]=c(x,y)
+      res[k,3]=f(corrs[[i]][[j]])
+      k=k+1
+    }
+  }
+  return(res)
+}
+
+
+#'
+#' @description  compute correlation with confidence intervals
+#    removes two first columns assumed as coordinates
+corrTest<-function(m){
+  if(is.na(m)){return(list(estimate=NA,conf.int.min=NA,conf.int.max=NA))}
+  m = m[,c(-1,-2)]
+  est=matrix(NA,ncol(m),ncol(m))
+  mi=matrix(NA,ncol(m),ncol(m))
+  ma=matrix(NA,ncol(m),ncol(m))
+  tryCatch({
+  for(i in 1:(ncol(m)-1)){
+    for(j in (i+1):ncol(m)){
+      if(length(which(is.na(m[,i])))<length(m[,i])-3&length(which(is.na(m[,j])))<length(m[,j])-3){
+        tt=cor.test(m[,i],m[,j])
+        est[i,j]=tt$estimate;mi[i,j]=tt$conf.int[1];ma[i,j]=tt$conf.int[2]
+        est[j,i]=tt$estimate;mi[j,i]=tt$conf.int[1];ma[j,i]=tt$conf.int[2]
+      }
+    }
+  }
+  for(i in 1:ncol(m)){if(length(which(is.na(m[,i])))<length(m[,i])-3){est[i,i]=1.0;mi[i,i]=1.0;ma[i,i]=1.0}}
+  },error=function(e){show(e);show(tt)})
+  return(list(estimate=est,conf.int.min=mi,conf.int.max=ma))
+}
+
+
+
+
+
+getLinearModels<-function(yvar,vars,nvars){
+  # brute force with enumerating all sets
+  varsets =list(c())
+  for(i in 1:nvars){
+    newsets = list();j=1
+    for(prevset in varsets){
+      for(var in vars){
+        newsets[[j]]=union(prevset,c(var));j=j+1
+      }
+    }
+    k=1;varsets=list()
+    for(j1 in 1:length(newsets)){
+      toadd=TRUE
+      if(length(varsets)>0){
+        for(j2 in 1:length(varsets)){
+          toadd=toadd&(!setequal(newsets[[j1]],varsets[[j2]]))
+        }
+      }
+      if(toadd){varsets[[k]]=newsets[[j1]];k=k+1}
+    }
+    #show(varsets)
+  }
+  res=c()
+  for(varset in varsets){
+    if(length(varset)<=nvars){
+      currentmodel=paste0(yvar,"~",varset[1])
+      if(length(varset)>1){for(var in varset[2:length(varset)]){currentmodel=paste0(currentmodel,"+",var)}}
+      res=append(res,currentmodel)
+    }
+  }
+  return(res)
+}
+
+
+
+
+
+
+
---
+++ Models/StaticCorrelations/mapFunctions.R
@@ -0,0 +1,85 @@
+
+# mapping functions
+
+library(cartography)
+library(classInt)
+
+#'
+#'
+loadIndicatorData<-function(file){
+  if(extension(file)=='.csv'){
+    raw=read.csv(file=file,sep=";",header=TRUE)
+    rows=apply(raw,1,function(r){prod(as.numeric(!is.na(r[c(1,2)])))>0})
+    return(as.tbl(raw[rows,]))
+  }
+  if(extension(file)=='.RData'){
+    load(file)
+    fullres = res[which(sapply(res,length)>1)];rm(res);gc()
+    nonares = fullres[which(sapply(fullres,function(l){length(which(is.na(l)))})<29)];rm(fullres);gc()
+    resdf = data.frame(matrix(unlist(nonares),nrow=length(nonares),byrow = T))
+    names(resdf)<-c('lonmin','latmin',names(nonares[[1]])[3:length(nonares[[1]])])
+    return(resdf) 
+   #return(as.tbl(resdf))
+  }
+}
+
+
+
+#y=function(x){log(x+0.01)};yinv = function(y){exp(y)-0.01}
+
+
+map<-function(indiccols,filename=NULL,width=0,height=0,mfrow=c(1,1),mar=c(2,2.5,1.5,2) + 0.1,sdata=sdata,indicnames=colnames(sdata)){
+  if(!is.null(filename)){
+    png(file=paste0(resdir,filename),width=width,height=height,units='cm',res=600)
+  }
+  par(mfrow=mfrow ,mar = mar,
+      oma = c(0,0,0,1) + 0.1)
+  cols <- carto.pal(pal1 = "green.pal",n1 = 5, pal2 = "red.pal",n2 = 5)
+  k=1
+  for(indic in indiccols){
+    #x = y(unlist(sdata[,indic]))
+    x = unlist(sdata[,indic])
+    m=acast(data.frame(sdata[,c(1,2)],x),latmin~lonmin);r = raster(m[seq(from=nrow(m),to=1,by=-1),])
+    crs(r)<-"+proj=longlat +datum=WGS84";extent(r)<-c(min(sdata$lonmin),max(sdata$lonmin),min(sdata$latmin),max(sdata$latmin))
+    breaks=classIntervals(x,10)
+    ticks=round(classIntervals(x,5,style = "equal")$brks,2)
+    #ticks = yinv(seq(round(minValue(r),digits=1),round(maxValue(r),digits=1), round((round(maxValue(r),digits=2) - round(minValue(r),digits=2))/5,digits=1)))
+    #ticks = seq(round(minValue(r),digits=1),round(maxValue(r),digits=1), round((round(maxValue(r),digits=2) - round(minValue(r),digits=2))/5,digits=1))
+    plot(r,main=indicnames[k],
+         col=cols,breaks=unique(breaks$brks),
+         legend.width = 1.5,
+         axis.args=list(cex.axis=0.7,at=ticks,labels=ticks)#,
+         #legend.args=list(at=ticks,labels=ticks,text='')
+    )
+    k=k+1
+  }
+  if(!is.null(filename)){
+    dev.off()
+  }
+}
+
+
+mapCorrs <- function(data,col,coordcols,filename,main=""){
+  png(file=paste0(resdir,filename),width=12,height=10,units='cm',res=600)
+  par(mar=c(2,2,2,1))
+  cols <- carto.pal(pal1 = "green.pal",n1 = 5, pal2 = "red.pal",n2 = 5)
+  x = unlist(data[,col])
+  m=acast(data.frame(data[,coordcols],x),lat~lon);r = raster(m[seq(from=nrow(m),to=1,by=-1),])
+  crs(r)<-"+proj=longlat +datum=WGS84";extent(r)<-c(min(data$lon),max(data$lon),min(data$lat),max(data$lat))
+  if(max(x)>min(x)){
+    breaks=classIntervals(x,10)
+    #ticks = seq(round(minValue(r),digits=1),round(maxValue(r),digits=1), round((round(maxValue(r),digits=2) - round(minValue(r),digits=2))/5,digits=1))
+    ticks = seq( minValue(r),maxValue(r),(maxValue(r)-minValue(r))/5)
+    ticks = round(ticks,digits=2)
+    plot(r,main=main,
+         col=cols,breaks=unique(breaks$brks),
+         legend.width = 1.5,
+         axis.args=list(at=ticks,labels=ticks,cex.axis=0.8)
+    )
+  }
+  dev.off()
+}
+
+
+
+
